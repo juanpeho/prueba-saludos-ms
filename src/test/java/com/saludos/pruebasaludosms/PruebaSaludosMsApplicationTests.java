ab6890a441b39a6c66f85340566a39cfe33ed457
Juan Camilo Perez Hoyos <jcperezh@sura.com.co>
2021-07-26 18:34:10 -0500

    Estructura inicial del proyecto

---
+++ src/test/java/com/saludos/pruebasaludosms/PruebaSaludosMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saludos.pruebasaludosms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaSaludosMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
